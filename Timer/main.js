e0d019e383d99074224af983636110b887e98d41
Antonio <antonio.nogueiraneto@icloud.com>
2022-11-11 22:36:45 -0300

    frist commit

---
+++ Timer/main.js
@@ -0,0 +1,43 @@
+function createHoursandSeconds(seconds){
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'GMT'
+  })
+}
+
+
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+let segundos = 0;
+let timer
+
+startClock = () => {
+  timer = setInterval( function(){
+    segundos++
+    relogio.innerHTML = createHoursandSeconds(segundos)
+  },1000)
+}
+
+
+document.addEventListener('click', function(e){
+ const el = e.target;
+ if ( el.classList.contains('zerar')){
+  relogio.classList.remove('pausado')
+  clearInterval(timer)
+  relogio.innerHTML = '00:00:00'
+  segundos = 0;
+ } else if (el.classList.contains('iniciar')){
+  relogio.classList.remove('pausado')
+  clearInterval(timer)
+  startClock()
+ } else if ( el.classList.contains('pausar')){
+  relogio.classList.add('pausado')
+  clearInterval(timer)
+ }
+})
+
+
+
